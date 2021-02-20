a3d867f46056c8a041460f738fc864a5cd2dee85
mingor <mmmmingor@gmail.com>
2021-02-20 10:08:28 +0000

    init repo

---
+++ README.md
@@ -0,0 +1,1 @@
+UCUS-SCE111 program4
---
+++ download.sh
@@ -0,0 +1,52 @@
+mkdir code
+pushd code
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/Listing.pdf
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/Listing.ps
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/Makefile
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/Makefile.dep
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/cxi.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/cxid.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/logstream.h
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/protocol.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/protocol.h
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/sockets.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/code/sockets.h
+popd
+
+mkdir dot.score
+pushd dot.score
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/SCORE
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/SCORE.pair
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/mk.build
+
+    mkdir local
+    pushd local
+    wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/local/localfile
+    wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/local/localtto
+    popd
+
+    mkdir remote
+    pushd remote
+    wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/remote/server1file
+    wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/dot.score/remote/to-remove
+    popd
+
+popd
+
+mkdir misc
+pushd misc
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/demangle.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/filesize.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/getenv.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/lsfile.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/makeunique.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/netbytes.cpp
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/misc/statfile.cpp
+popd
+
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/Makefile
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/asg4-client-server.mm
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/asg4-client-server.pdf
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/asg4-client-server.ps
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/asg4-client-server.qrf
+wget https://www2.ucsc.edu/courses/cse111-wm/:/Assignments/asg4-client-server/asg4-client-server.tt
